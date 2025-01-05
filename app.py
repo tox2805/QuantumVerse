8e276a136306756022e2c3243a03bf038d1cbe93
Tom <thomas.oxley@live.co.uk>
2025-01-05 01:23:08 +0000

    inital commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Home page route
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# Quantum Level page route
+@app.route('/quantumlevel')
+def quantum_level():
+    return render_template('quantumlevel.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

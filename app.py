14ca0612cadfb7ff06409cffce8d622d20ad5af2
Tom <thomas.oxley@live.co.uk>
2025-01-05 03:15:45 +0000

    fixing production

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -12,8 +13,9 @@ def home():
 def quantum_level():
     return render_template('quantumlevel.html')
 
+app.config['ENV'] = 'production'
+app.config['DEBUG'] = False 
+
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.config['ENV'] = 'production'
-    app.config['DEBUG'] = False 
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Get the port from environment or default to 5000
+    app.run(host='0.0.0.0', port=port)  # Bind to 0.0.0.0 for production deployment
